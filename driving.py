c4e8d03404c69384e110fe54167872ca561713c0
Addison Chen <mracn0630@gmail.com>
2019-11-05 23:02:26 +0800

    TW US driver age

---
+++ driving.py
@@ -0,0 +1,14 @@
+#台灣及美國考取汽車駕照年齡
+country = input('請問你是哪國人？')
+age = input('請輸入你幾歲？')
+age = int(age)
+if country == '台灣':
+    if age < 18:
+        print('你還不能考駕照')
+    else:
+        print('你可以考駕照')
+elif country =='美國':
+    if age >= 16:
+        print('你們可以考駕照')
+    else:
+        print('你還不能考駕照')
